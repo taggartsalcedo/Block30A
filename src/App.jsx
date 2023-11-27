ad7673327ebe0266926119d4e0ea9f6ee9e6899e
Taggart <t.salcedo@hotmail.com>
2023-11-27 09:54:08 -0500

    app.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import bookLogo from './assets/books.png'
 import Accounts from './components/Accounts.jsx'
 import Books from './components/Books.jsx'
 import Login from './components/login.jsx'
@@ -9,11 +10,36 @@ import SingleBook from './components/SingleBook.jsx'
 
 function App() {
   const [token, setToken] = useState(null)
-  const [books, setBooks] = useState([false])
+  const [books, setBooks] = useState(false)
+  const [register, setRegister] = useState(false)
+
 
   return (
     <>
-    
+      <h1><img id='logo-image' src={bookLogo} />Library App</h1>
+      {
+        register ? (
+          <Login />) : (
+          <Register token={token}
+            setToken={setToken}
+            setRegister={setRegister}
+            />
+          )
+      }
+
+      <h1>Books</h1>
+      {
+        homeLibrary ? (
+          <SingleBook
+          thisBook={thisBook}
+          setHomeLibrary={setHomeLibrary}
+          />) : (
+          <Books
+          setHomeLibrary={setHomeLibrary}
+          setThisBook={setThisBook}
+          />
+        )
+      }
     </>
   )
 }
