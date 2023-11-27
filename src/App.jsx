69ecd9e89ce4b680c80205179b0b602bd8b2e213
Taggart <t.salcedo@hotmail.com>
2023-11-27 09:09:32 -0500

    Login.jsx complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
-import bookLogo from './assets/books.png'
+import Accounts from './components/Accounts.jsx'
+import Books from './components/Books.jsx'
+import Login from './components/login.jsx'
+import Navigation from './components/navigation.jsx'
+import Register from './components/Register.jsx'
+import SingleBook from './components/SingleBook.jsx'
+
 
 function App() {
   const [token, setToken] = useState(null)
+  const [books, setBooks] = useState([false])
 
   return (
     <>
-      <h1><img id='logo-image' src={bookLogo}/>Library App</h1>
-
-      <p>Complete the React components needed to allow users to browse a library catalog, check out books, review their account, and return books that they've finished reading.</p>
-
-      <p>You may need to use the `token` in this top-level component in other components that need to know if a user has logged in or not.</p>
-
-      <p>Don't forget to set up React Router to navigate between the different views of your single page application!</p>
+    
     </>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,1 +1,50 @@
 /* TODO - add your code to create a functional React component that renders a login form */
+import { useState } from 'react'
+
+const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(null)
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        try {
+            const responce = await fetch ('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login',
+                {
+                    method: "Post",
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email: email,
+                        password: password
+                    })
+                })
+                const result = await responce.json()
+                console.log(result)
+            } catch (error) {
+                setError(error.message)
+            }
+    }
+
+return (
+    <>
+        <h3>Log On</h3>
+        {error && <p>{error}</p>}
+        <form onSubmit={handleSubmit}>
+            <label>
+                email: <input value={email} onChange={(event) => 
+                setEmail(event.target.value)}/>
+            </label>
+            <label>
+                password: <input value={password} onChange={(event) =>
+                setPassword(event.target.value)}/>
+            </label>
+            <button>Log On</button>
+        </form>
+    </>
+)
+
+}
+
+export default Login
